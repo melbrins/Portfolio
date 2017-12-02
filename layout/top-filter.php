8ea4c0a880b135cf9d390161acc28594052fda19
Anthony Pucelle <info@melbrins.com>
2017-12-02 23:41:28 +0000

    fix portfolio filter position on mobile

--- layout/top-filter.php
+++ layout/top-filter.php
@@ -2,16 +2,16 @@
 	<div class="wrap">
 		<span class="current-filter">All</span>
 		<a id="toggle-filters"><i class="fa fa-filter" aria-hidden="true"></i>Filter</a>
+		<div class="heading">
+			<ul id="filters" class="clearfix">
+				<li><span class="filter active" data-filter="all">All</span></li>
+				<li><span class="filter" data-filter=".illustration">Illustration</span></li>
+				<li><span class="filter" data-filter=".web">Website</span></li>
+				<li><span class="filter" data-filter=".print">Print</span></li>
+				<li><span class="filter" data-filter=".thered">3D</span></li>
+				<!-- <li><span class="filter" data-filter=".wip">WIP</span></li> -->
+			</ul>
+		</div>
 	</div>
 </div>
 
-<div class="heading">
-	<ul id="filters" class="clearfix">
-		<li><span class="filter active" data-filter="all">All</span></li>
-		<li><span class="filter" data-filter=".illustration">Illustration</span></li>
-		<li><span class="filter" data-filter=".web">Website</span></li>
-		<li><span class="filter" data-filter=".print">Print</span></li>
-		<li><span class="filter" data-filter=".thered">3D</span></li>
-		<!-- <li><span class="filter" data-filter=".wip">WIP</span></li> -->
-	</ul>
-</div>
