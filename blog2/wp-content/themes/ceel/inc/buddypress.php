b3c5da4cef1fae140be36ea2b9154a0f0593bc9b
Anthony Pucelle <anthony@presentagency.com>
2018-08-10 14:07:19 +0100

    remove blog

--- blog2/wp-content/themes/ceel/inc/buddypress.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * BuddyPress Compatibility
- */
-
-function ceel_bp_css() {
-    wp_enqueue_style( 'ceel-buddypress', get_template_directory_uri() . '/css/buddypress.css' );
-}
-add_action( 'wp_enqueue_scripts', 'ceel_bp_css' );
